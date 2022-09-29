9427b862cdf34528939663c7f800f0946cd4338d
philfuster <pafus23@protonmail.com>
2022-09-29 19:12:44 -0400

    Read the readme for the list-items-controller exercise. Finished the exercise and watched the solution video

--- src/routes/__tests__/list-items-controller.exercise.js
+++ src/routes/__tests__/list-items-controller.exercise.js
@@ -1,45 +1,250 @@
 // Testing Controllers
 
-// 🐨 you'll need a few of the generaters from test/utils/generate.js
-// 💰 remember, you can import files in the test/utils directory as if they're node_modules
-// 💰 import * as generate from 'utils/generate'
+import {
+	buildNext,
+	buildReq,
+	buildRes,
+	buildListItem,
+	buildBook,
+	buildUser,
+	notes,
+} from 'utils/generate';
 
-// 🐨 getListItem calls `expandBookData` which calls `booksDB.readById`
-// so you'll need to import the booksDB from '../../db/books'
-// 💰 import * as booksDB from '../../db/books'
+import * as booksDB from '../../db/books';
 
-// 🐨 don't forget to import the listItemsController from '../list-items-controller'
-// here, that's the thing we're testing afterall :)
-// 💰 import * as listItemsController from '../list-items-controller'
-
-// 🐨 use jest.mock to mock '../../db/books' because we don't actually want to make
+import * as listItemsController from '../list-items-controller';
+import * as listItemsDB from '../../db/list-items';
 // database calls in this test file.
+jest.mock('../../db/books');
+jest.mock('../../db/list-items');
 
-// 🐨 ensure that all mock functions have their call history cleared using
-// jest.clearAllMocks here as in the example.
+beforeEach(() => {
+	jest.clearAllMocks();
+});
 
 test('getListItem returns the req.listItem', async () => {
-  // 🐨 create a user
-  //
-  // 🐨 create a book
-  //
-  // 🐨 create a listItem that has the user as the owner and the book
-  // 💰 const listItem = buildListItem({ownerId: user.id, bookId: book.id})
-  //
-  // 🐨 mock booksDB.readById to resolve to the book
-  // 💰 use mockResolvedValueOnce
-  //
-  // 🐨 make a request object that has properties for the user and listItem
-  // 💰 checkout the implementation of getListItem in ../list-items-controller
-  // to see how the request object is used and what properties it needs.
-  // 💰 and you can use buildReq from utils/generate
-  //
-  // 🐨 make a response object
-  // 💰 just use buildRes from utils/generate
-  //
-  // 🐨 make a call to getListItem with the req and res (`await` the result)
-  //
-  // 🐨 assert that booksDB.readById was called correctly
-  //
-  //🐨 assert that res.json was called correctly
-})
+	const user = buildUser();
+	const book = buildBook();
+	const listItem = buildListItem({ ownerId: user.id, bookId: book.id });
+	//
+	booksDB.readById.mockResolvedValueOnce(book);
+	//
+	const req = buildReq({ user, listItem });
+	const res = buildRes();
+	await listItemsController.getListItem(req, res);
+	//
+	expect(booksDB.readById).toHaveBeenCalledWith(book.id);
+	expect(booksDB.readById).toHaveBeenCalledTimes(1);
+	expect(res.json).toHaveBeenCalledWith({ listItem: { ...listItem, book } });
+	expect(res.json).toHaveBeenCalledTimes(1);
+});
+
+test(`getListItems returns a user's list items`, async () => {
+	const user = buildUser();
+	const booksList = [buildBook(), buildBook()];
+	const userListItems = [
+		buildListItem({ ownerId: user.id, bookId: booksList[0].id }),
+		buildListItem({ ownerId: user.id, bookId: booksList[1].id }),
+	];
+	//
+	booksDB.readManyById.mockResolvedValueOnce(booksList);
+	listItemsDB.query.mockResolvedValueOnce(userListItems);
+	//
+	const req = buildReq({ user });
+	const res = buildRes();
+	await listItemsController.getListItems(req, res);
+	//
+	expect(booksDB.readManyById).toHaveBeenCalledWith([booksList[0].id, booksList[1].id]);
+	expect(booksDB.readManyById).toHaveBeenCalledTimes(1);
+	expect(listItemsDB.query).toHaveBeenCalledWith({ ownerId: req.user.id });
+	expect(listItemsDB.query).toHaveBeenCalledTimes(1);
+	expect(res.json).toHaveBeenCalledWith({
+		listItems: [
+			{
+				...userListItems[0],
+				book: booksList[0],
+			},
+			{ ...userListItems[1], book: booksList[1] },
+		],
+	});
+	expect(res.json).toHaveBeenCalledTimes(1);
+});
+
+test('createListItem returns a 400 error if no bookId is provided', async () => {
+	const req = buildReq();
+	const res = buildRes();
+	await listItemsController.createListItem(req, res);
+	expect(res.status).toHaveBeenCalledWith(400);
+	expect(res.status).toHaveBeenCalledTimes(1);
+
+	expect(res.json.mock.calls[0]).toMatchInlineSnapshot(
+		res.json.mock.calls[0],
+		`
+    Object {
+      "0": Object {
+        "message": "No bookId provided",
+      },
+    }
+  `,
+	);
+	expect(res.json).toHaveBeenCalledTimes(1);
+});
+
+test('createListItem returns 400 if the user already has a list item for the given book', async () => {
+	const user = buildUser({ id: 'FAKE_USER_ID' });
+	const book = buildBook({ id: 'FAKE_BOOK_ID' });
+
+	const existingListItem = buildListItem({ ownerId: user.id, bookId: book.id });
+	listItemsDB.query.mockResolvedValueOnce([existingListItem]);
+
+	const req = buildReq({ user, body: { bookId: book.id } });
+	const res = buildRes();
+
+	await listItemsController.createListItem(req, res);
+
+	expect(listItemsDB.query).toHaveBeenCalledWith({
+		ownerId: user.id,
+		bookId: book.id,
+	});
+	expect(listItemsDB.query).toHaveBeenCalledTimes(1);
+
+	expect(res.status).toHaveBeenCalledWith(400);
+	expect(res.status).toHaveBeenCalledTimes(1);
+	expect(res.json.mock.calls[0]).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "message": "User FAKE_USER_ID already has a list item for the book with the ID FAKE_BOOK_ID",
+      },
+    ]
+  `);
+});
+
+test('createListItem creates and returns the a listItem', async () => {
+	const user = buildUser();
+	const book = buildBook();
+	const createdListItem = buildListItem({ ownerId: user.id, bookId: book.id });
+
+	listItemsDB.query.mockResolvedValueOnce([]);
+	listItemsDB.create.mockResolvedValueOnce({ ...createdListItem });
+	booksDB.readById.mockResolvedValueOnce(book);
+
+	const req = buildReq({ user, body: { bookId: book.id } });
+	const res = buildRes();
+	await listItemsController.createListItem(req, res);
+	expect(listItemsDB.query).toHaveBeenCalledWith({
+		ownerId: user.id,
+		bookId: book.id,
+	});
+	expect(listItemsDB.query).toHaveBeenCalledTimes(1);
+
+	expect(listItemsDB.create).toHaveBeenCalledWith({
+		ownerId: user.id,
+		bookId: book.id,
+	});
+	expect(listItemsDB.create).toHaveBeenCalledTimes(1);
+
+	expect(booksDB.readById).toHaveBeenCalledWith(book.id);
+	expect(booksDB.readById).toHaveBeenCalledTimes(1);
+
+	expect(res.status).not.toHaveBeenCalled();
+	expect(res.json).toHaveBeenCalledWith({ listItem: { ...createdListItem, book } });
+});
+
+test('setListItem returns a 404 error if listItem does not exist', async () => {
+	const fakeListItemId = 'FAKE_LIST_ITEM_ID';
+	const req = buildReq({ params: { id: fakeListItemId } });
+	const res = buildRes();
+	const next = buildNext();
+	listItemsDB.readById.mockResolvedValueOnce(null);
+	await listItemsController.setListItem(req, res, next);
+	expect(next).not.toHaveBeenCalled();
+	expect(res.status).toHaveBeenCalledWith(404);
+	expect(res.status).toHaveBeenCalledTimes(1);
+	expect(res.json.mock.calls[0]).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "message": "No list item was found with the id of FAKE_LIST_ITEM_ID",
+      },
+    ]
+  `);
+	expect(res.json).toHaveBeenCalledTimes(1);
+});
+
+test('setListItem returns a 403 error if the list item does not belong to the user', async () => {
+	const user = buildUser({ id: 'FAKE_USER_ID' });
+	// listItem.ownerId is pretty much guaranteed to be different from
+	// the userId of the user generated above.
+	const listItem = buildListItem();
+	const req = buildReq({ user, params: { id: 'FAKE_LIST_ITEM_ID' } });
+	const res = buildRes();
+	const next = buildNext();
+	listItemsDB.readById.mockResolvedValueOnce(listItem);
+	await listItemsController.setListItem(req, res, next);
+	expect(res.status).toHaveBeenCalledWith(403);
+	expect(res.status).toHaveBeenCalledTimes(1);
+	expect(res.json.mock.calls[0]).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "message": "User with id FAKE_USER_ID is not authorized to access the list item FAKE_LIST_ITEM_ID",
+      },
+    ]
+  `);
+	expect(res.json).toHaveBeenCalledTimes(1);
+});
+
+test('setListItem sets listItem on the req', async () => {
+	const user = buildUser();
+	const listItem = buildListItem({ ownerId: user.id });
+
+	listItemsDB.readById.mockResolvedValueOnce(listItem);
+
+	const req = buildReq({ user, params: { id: listItem.id } });
+	const res = buildRes();
+	const next = buildNext();
+
+	await listItemsController.setListItem(req, res, next);
+
+	expect(listItemsDB.readById).toHaveBeenCalledWith(listItem.id);
+	expect(listItemsDB.readById).toHaveBeenCalledTimes(1);
+	expect(res.json).not.toHaveBeenCalled();
+	expect(next).toHaveBeenLastCalledWith(/* nothing! */);
+	expect(next).toHaveBeenCalledTimes(1);
+});
+
+test('updateListItem updates an existing list item', async () => {
+	const user = buildUser();
+	const book = buildBook();
+	const originalListItem = buildListItem({ ownerId: user.id, bookId: book.id });
+	const updates = { notes: notes() };
+	const mergedListItemsAndUpdates = { ...originalListItem, ...updates };
+
+	listItemsDB.update.mockResolvedValueOnce(mergedListItemsAndUpdates);
+	booksDB.readById.mockResolvedValueOnce(book);
+
+	const req = buildReq({ user, listItem: originalListItem, body: { ...updates } });
+	const res = buildRes();
+
+	await listItemsController.updateListItem(req, res);
+
+	expect(listItemsDB.update).toHaveBeenCalledWith(originalListItem.id, updates);
+	expect(listItemsDB.update).toHaveBeenCalledTimes(1);
+	expect(booksDB.readById).toHaveBeenCalledWith(book.id);
+	expect(booksDB.readById).toBeCalledTimes(1);
+
+	expect(listItemsDB.update).toHaveBeenCalledWith(req.listItem.id, req.body);
+	expect(res.json).toHaveBeenCalledWith({ listItem: { ...mergedListItemsAndUpdates, book } });
+	expect(res.json).toHaveBeenCalledTimes(1);
+});
+
+test('deleteListItem deletes an existing list item', async () => {
+	const user = buildUser();
+	const listItem = buildListItem({ ownerId: user.id });
+	const req = buildReq({ listItem: { id: listItem.id } });
+	const res = buildRes();
+	await listItemsController.deleteListItem(req, res);
+	expect(listItemsDB.remove).toHaveBeenCalledWith(listItem.id);
+	expect(listItemsDB.remove).toHaveBeenCalledTimes(1);
+	expect(res.status).not.toHaveBeenCalled();
+	expect(res.json).toHaveBeenCalledWith({ success: true });
+	expect(res.json).toHaveBeenCalledTimes(1);
+});
--- src/utils/__tests__/error-middleware.exercise.js
+++ src/utils/__tests__/error-middleware.exercise.js
@@ -1,72 +1,85 @@
 // Testing Middleware
 
 // 🐨 you'll need both of these:
-import {UnauthorizedError} from 'express-jwt'
-import errorMiddleware from '../error-middleware'
+import { UnauthorizedError } from 'express-jwt';
+import { buildRes, buildReq, buildNext } from 'utils/generate';
+import errorMiddleware from '../error-middleware';
+
+// My solution to extra credit 1. Might have been a hasty abstraction
+// in comparison to the course solution
+// const setup = (overrides = {}) => {
+//   const req = {}
+//   const next = jest.fn()
+//   const res = {
+//     json: jest.fn(() => res),
+//     status: jest.fn(() => res),
+//   }
+//   const error = new Error('blah')
+//   return {
+//     req,
+//     res,
+//     next,
+//     error,
+//     ...overrides,
+//   }
+// }
+
+// course solution to extra credit 1.
+// const buildRes = overrides => {
+//   const res = {
+//     json: jest.fn(() => res),
+//     status: jest.fn(() => res),
+//     ...overrides,
+//   }
+//   return res
+// }
 
 // 🐨 Write a test for the UnauthorizedError case
 describe('errorMiddleware', () => {
-  // shared variables
-  //
-  test('responds with 401 for express-jwt UnauthorizedError', () => {
-    const req = {}
-    const next = jest.fn()
-    const code = 'some_error_code'
-    const message = 'Some message'
-    const res = {
-      json: jest.fn(() => res),
-      status: jest.fn(() => res),
-    }
-    const error = new UnauthorizedError(code, {
-      message,
-    })
-    errorMiddleware(error, req, res, next)
-    expect(next).not.toHaveBeenCalled()
-    expect(res.json).toHaveBeenCalledTimes(1)
-    expect(res.json).toHaveBeenCalledWith({
-      code: error.code,
-      message: error.message,
-    })
-    expect(res.status).toHaveBeenCalledWith(401)
-    expect(res.status).toHaveBeenCalledTimes(1)
-  })
+	//
+	test('responds with 401 for express-jwt UnauthorizedError', () => {
+		const req = buildReq();
+		const res = buildRes();
+		const next = buildNext();
+		const error = new UnauthorizedError('error_code', { message: 'Some message' });
+		errorMiddleware(error, req, res, next);
+		expect(next).not.toHaveBeenCalled();
+		expect(res.json).toHaveBeenCalledTimes(1);
+		expect(res.json).toHaveBeenCalledWith({
+			code: error.code,
+			message: error.message,
+		});
+		expect(res.status).toHaveBeenCalledTimes(1);
+		expect(res.status).toHaveBeenCalledWith(401);
+	});
 
-  // 🐨 Write a test for the headersSent case
-  test('calls next if headersSent is true', () => {
-    const req = {}
-    const next = jest.fn()
-    const code = 'some_error_code'
-    const message = 'Some message'
-    const error = new Error(code, {message})
-    const res = {
-      json: jest.fn(() => res),
-      status: jest.fn(() => res),
-      headersSent: jest.fn(() => res),
-    }
-    errorMiddleware(error, req, res, next)
-    expect(res.json).not.toHaveBeenCalled()
-    expect(res.status).not.toHaveBeenCalled()
-    expect(next).toHaveBeenCalledTimes(1)
-    expect(next).toHaveBeenCalledWith(error)
-  })
+	// 🐨 Write a test for the headersSent case
+	test('calls next if headersSent is true', () => {
+		const req = buildReq();
+		const res = buildRes({ headersSent: true });
+		const next = buildNext();
+		const error = new Error('blah');
+		errorMiddleware(error, req, res, next);
+		expect(res.json).not.toHaveBeenCalled();
+		expect(res.status).not.toHaveBeenCalled();
+		expect(next).toHaveBeenCalledTimes(1);
+		expect(next).toHaveBeenCalledWith(error);
+	});
 
-  test('responds with status code 500', () => {
-    const req = {}
-    const next = jest.fn()
-    const error = new Error('blah')
-    const res = {
-      json: jest.fn(() => res),
-      status: jest.fn(() => res),
-    }
-    errorMiddleware(error, req, res, next)
-    expect(next).not.toHaveBeenCalled()
-    expect(res.status).toHaveBeenCalledTimes(1)
-    expect(res.status).toHaveBeenCalledWith(500)
-    // not production env... expect a stack trace...
-    expect(res.json).toHaveBeenCalledTimes(1)
-    expect(res.json).toHaveBeenCalledWith({
-      message: error.message,
-      stack: error.stack,
-    })
-  })
-})
+	test('responds with status code 500', () => {
+		const req = buildReq();
+		const res = buildRes();
+		const next = buildNext();
+		const error = new Error('blah');
+		errorMiddleware(error, req, res, next);
+		expect(next).not.toHaveBeenCalled();
+		expect(res.status).toHaveBeenCalledTimes(1);
+		expect(res.status).toHaveBeenCalledWith(500);
+		// not production env... expect a stack trace...
+		expect(res.json).toHaveBeenCalledTimes(1);
+		expect(res.json).toHaveBeenCalledWith({
+			message: error.message,
+			stack: error.stack,
+		});
+	});
+});
